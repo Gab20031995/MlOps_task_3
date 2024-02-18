d1c0d60749a5bc101ff7802594a81803259c4800
Gab30031995 <luizclzm@gmail.com>
2024-02-18 04:18:41 -0600

    Second commit

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def suma(a,b):
-    return a+b
+def suma(a, b):
+    return a + b
 
 if __name__ == '__main__':
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
 
-    result = suma(num1,num2)
-    print(f"El sesultados de la suma de {num1} y {num2} es:", result)
-
+    result = suma(num1, num2)
+    print(f"El resultado de la suma de {num1} y {num2} es:", result)
